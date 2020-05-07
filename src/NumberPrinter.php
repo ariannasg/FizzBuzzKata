b39350fec416cf2cdd18da019e5e389e48042d7b
Ari <ariannasg.ldn@gmail.com>
2020-05-07 08:48:08 +0100

    Refactored simple test

---
+++ src/NumberPrinter.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+namespace FizzBuzz;
+
+class NumberPrinter
+{
+
+    public static function printNumber(int $number): int
+    {
+        return $number;
+    }
+}
--- tests/NumberPrinterTest.php
+++ tests/NumberPrinterTest.php
@@ -2,18 +2,15 @@
 
 namespace FizzBuzz\Test;
 
+use FizzBuzz\NumberPrinter;
 use PHPUnit\Framework\TestCase;
 
 class NumberPrinterTest extends TestCase
 {
-    public function testWeCanReturnTheSameNumber(): void {
-        $result = $this->printNumber(1);
+    public function testWeCanReturnTheSameNumber(): void
+    {
+        $result = NumberPrinter::printNumber(1);
 
         self::assertEquals(1, $result, 'When taking 1 we should return 1');
-    }
-
-    private function printNumber(int $number): int
-    {
-        return $number;
     }
 }
