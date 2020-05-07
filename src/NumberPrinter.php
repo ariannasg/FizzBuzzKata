8f552f81a6b46da185d2c1347dc6f71118d113f8
Ari <ariannasg.ldn@gmail.com>
2020-05-07 10:07:38 +0100

    Refactored to use constants in the NumberPrinter class

--- src/NumberPrinter.php
+++ src/NumberPrinter.php
@@ -4,18 +4,22 @@ namespace FizzBuzz;
 
 class NumberPrinter
 {
+    private const MULTIPLE_OF_THREE_AND_FIVE = 'FizzBuzz';
+    private const MULTIPLE_OF_THREE = 'Fizz';
+    private const MULTIPLE_OF_FIVE = 'Buzz';
+
     public static function execute(int $number): string
     {
         if (($number % 3 === 0) && ($number % 5 === 0)) {
-            return 'FizzBuzz';
+            return self::MULTIPLE_OF_THREE_AND_FIVE;
         }
 
         if ($number % 3 === 0) {
-            return 'Fizz';
+            return self::MULTIPLE_OF_THREE;
         }
 
         if ($number % 5 === 0) {
-            return 'Buzz';
+            return self::MULTIPLE_OF_FIVE;
         }
 
         return (string)$number;
