9f21f75d71e1ea1e65780c47807fa3380e8a66bf
Ari <ariannasg.ldn@gmail.com>
2020-05-07 14:21:13 +0100

    Removed unnecessary vars from test

--- tests/NumberPrinterCommandTest.php
+++ tests/NumberPrinterCommandTest.php
@@ -5,8 +5,6 @@ namespace FizzBuzz\Test;
 use FizzBuzz\NumberPrinter;
 use FizzBuzz\NumberPrinterCommand;
 use PHPUnit\Framework\TestCase;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
 
 class NumberPrinterCommandTest extends TestCase
 {
@@ -14,12 +12,9 @@ class NumberPrinterCommandTest extends TestCase
     {
         $numberPrinter = $this->getMockBuilder(NumberPrinter::class)->getMock();
 
-        $input = $this->getMockBuilder(InputInterface::class)->getMock();
-        $output = $this->getMockBuilder(OutputInterface::class)->getMock();
-
         $numberPrinter->expects(self::once())->method('execute');
 
         $command = new NumberPrinterCommand($numberPrinter);
-        $command->execute($input, $output);
+        $command->execute();
     }
 }
